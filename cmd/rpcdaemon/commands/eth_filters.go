ad6eed7f5e21dc3d3d36df3353cc9626706255b7
0xdapper <0xdapp@protonmail.com>
2022-03-13 12:50:57 +0530

    eth_filters: add NewPendingTransactionsComplete

--- cmd/rpcdaemon/commands/eth_filters.go
+++ cmd/rpcdaemon/commands/eth_filters.go
@@ -110,3 +110,42 @@ func (api *APIImpl) NewPendingTransactions(ctx context.Context) (*rpc.Subscripti
 
 	return rpcSub, nil
 }
+
+// NewPendingTransactionsComplete send a notification each time a new (header) block is appended to the chain.
+func (api *APIImpl) NewPendingTransactionsComplete(ctx context.Context) (*rpc.Subscription, error) {
+	if api.filters == nil {
+		return &rpc.Subscription{}, rpc.ErrNotificationsUnsupported
+	}
+	notifier, supported := rpc.NotifierFromContext(ctx)
+	if !supported {
+		return &rpc.Subscription{}, rpc.ErrNotificationsUnsupported
+	}
+
+	rpcSub := notifier.CreateSubscription()
+
+	go func() {
+		defer debug.LogPanic()
+		txsCh := make(chan []types.Transaction, 1)
+		defer close(txsCh)
+		id := api.filters.SubscribePendingTxs(txsCh)
+		defer api.filters.UnsubscribePendingTxs(id)
+
+		for {
+			select {
+			case txs := <-txsCh:
+				for _, t := range txs {
+					if t != nil {
+						err := notifier.Notify(rpcSub.ID, t)
+						if err != nil {
+							log.Warn("error while notifying subscription", "err", err)
+						}
+					}
+				}
+			case <-rpcSub.Err():
+				return
+			}
+		}
+	}()
+
+	return rpcSub, nil
+}
